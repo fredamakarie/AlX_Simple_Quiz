fad4fd26e1496028a392be711e8444459491d4ca
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-06-13 17:00:54 +0100

    simple calculator

---
+++ calculator.js
@@ -0,0 +1,44 @@
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+document.getElementById('add').addEventListener('click', function() {
+    const num1 = parseFloat(document.getElementById('number1').value) || 0;
+    const num2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(num1, num2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('subtract').addEventListener('click', function() {
+    const num1 = parseFloat(document.getElementById('number1').value) || 0;
+    const num2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = subtract(num1, num2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function() {
+    const num1 = parseFloat(document.getElementById('number1').value) || 0;
+    const num2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = multiply(num1, num2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('divide').addEventListener('click', function() {
+    const num1 = parseFloat(document.getElementById('number1').value) || 0;
+    const num2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = divide(num1, num2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
